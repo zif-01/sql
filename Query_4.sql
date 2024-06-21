c5cc6aeafbc67d3599b06938098bbed28f0e845a
zif-01 <159900483+zif-01@users.noreply.github.com>
2024-06-21 15:53:51 +0700

    Add files via upload

--- Query_4.sql
+++ Query_4.sql
@@ -1,48 +1,120 @@
-/*Декартово произведение таблиц*/
+/*Вложенные запросы*/
+
+/*Применение простейшего подзапроса*/
+
+SELECT 
+  * 
+FROM 
+  students 
+WHERE 
+  idgroups = (
+    select 
+      idgroups 
+    from 
+      `groups` 
+    where 
+      napr = 'Педагогическое образование (с двумя профилями подготовки)' 
+      and prof = 'Математика и Информатика'
+  );
+  
+  /*Агрегатные функции в подзапросах*/
+  SELECT 
+    *
+FROM
+    discipl
+WHERE
+    hours > (SELECT 
+            AVG(hours)
+        FROM
+            discipl
+        WHERE
+            knamedisc LIKE '%Б%');
+		
+  /*Оператор IN в подзапросах*/
 SELECT 
     *
 FROM
-    students,
-    `groups`;
-    
-/*Соединение таблиц по общему значению*/
-SELECT 
-    `name`, napr, prof
-FROM
-    students,
     `groups`
 WHERE
-    students.idgroups = `groups`.idgroups;
-    
-/*Альтернативная форма создания таблиц c использованием общих полей(USING())*/
+    idgroups IN (SELECT 
+            idgroups
+        FROM
+            `groups`
+        WHERE
+            napr = 'Педагогическое образование (с двумя профилями подготовки)');
+            
 SELECT 
-    `name`, napr, prof
+    *
 FROM
-    students
-        JOIN
-    `groups` USING (idgroups);
-    
-SELECT 
-    `name`, napr, prof
-FROM
-    students
-        LEFT JOIN
-    `groups` USING (idgroups);
-    
-SELECT 
-    `name`, gr
-FROM
-    students
-        RIGHT JOIN
-    `groups` USING (idgroups)
-ORDER BY `name`;
-
-
-SELECT 
-    `name`, napr, prof
-FROM
-    students
-        JOIN
-    `groups` ON (students.idgroups = `groups`.idgroups)
+    discipl
 WHERE
-    napr LIKE 'И%';
+    hours IN (SELECT 
+            hours
+        FROM
+            discipl
+        WHERE
+            knamedisc LIKE '%Б%');
+            
+/*Связанные (коррелированные) подзапросы*/
+SELECT 
+    *
+FROM
+    `groups` `outer`
+WHERE
+    'Суббота' IN (SELECT 
+            den_ned
+        FROM
+            sched `inner`
+        WHERE
+            `inner`.idgroups = `outer`.idgroups);
+            
+/*Пример связанного подзапроса с агрегатной функцией*/
+SELECT 
+    gr as 'Группа'
+FROM
+    `groups` main
+WHERE
+    1 < (SELECT 
+            COUNT(*)
+        FROM
+            sched
+        WHERE
+            iddiscpl = main.idgroups);
+            
+/*Связывание таблицы с собой */
+
+SELECT 
+    *
+FROM
+    discipl main
+WHERE
+    hours > (SELECT 
+            AVG(hours)
+        FROM
+            discipl
+        WHERE
+            knamedisc = main.knamedisc)
+            
+SELECT 
+    *
+FROM
+    discipl main
+WHERE
+    hours > (SELECT 
+            (AVG(hours) * 1.5)
+        FROM
+            discipl
+        WHERE
+            knamedisc = main.knamedisc)
+            
+/*Использование условий группировки в связанных запросах */
+
+SELECT 
+    kaf, knamedisc, SUM(hours)
+FROM
+    discipl a
+GROUP BY kaf, knamedisc
+HAVING SUM(hours) > (SELECT
+    36 + MIN(hours)
+    FROM discipl
+    WHERE a.kaf = kaf);
